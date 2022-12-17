5b40eedb98790e9b8f0692956901e08e95f5dd49
Rafael Rodrigues Bento <rafael.rodrigues.bento@outlook.com>
2022-12-17 17:50:00 -0300

    TypeScript no formulário

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,15 +15,24 @@ import {
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
+  owner: zod.string().optional(),
   minutesAmount: zod.number().min(5).max(60),
 })
 
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm({
+  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
   })
 
-  function handleCreateNewCycle() {}
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    console.log(data)
+  }
 
   const task = watch('task')
   const isSubmitDisabled = !task
